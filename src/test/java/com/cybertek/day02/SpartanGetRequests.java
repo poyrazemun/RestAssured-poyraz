3bd80dd4fef81e31db07f9a34a58fabaa24e3000
poyrazemun <ofatih-poyraz@hotmail.com>
2022-07-11 18:53:42 +0200

    day02 Assertions

--- src/test/java/com/cybertek/day02/SpartanGetRequests.java
+++ src/test/java/com/cybertek/day02/SpartanGetRequests.java
@@ -4,6 +4,7 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class SpartanGetRequests {
@@ -17,7 +18,7 @@ public class SpartanGetRequests {
 
 
     @Test
-    public void test1(){
+    public void test1() {
 
         Response response = RestAssured.given().accept(ContentType.JSON)
                 .when()
@@ -37,15 +38,36 @@ public class SpartanGetRequests {
 
         //how to do API testing ?
 
-        Assertions.assertEquals(response.statusCode(),200);
+        Assertions.assertEquals(response.statusCode(), 200);
 
-        Assertions.assertEquals(response.contentType(),"application/json");
+        Assertions.assertEquals(response.contentType(), "application/json");
+
+    }
+
+    /*
+        Given accept header is application/json
+        When users sends a get request to /api/spartans/3
+        Then status code should be 200
+        And content type should be application/json
+        and json body should contain Fidole
+     */
+
+    @DisplayName("GET one spartan /api/spartans/3 and verify")
+    @Test
+    public void test2() {
 
 
+        Response response = RestAssured.given().accept(ContentType.JSON).when().get(baseUrl + "/api/spartans/3");
 
+        Assertions.assertEquals(response.statusCode(), 200);
+
+        Assertions.assertEquals(response.contentType(), "application/json");
+
+        Assertions.assertTrue(response.body().asString().contains("Fidole"));
 
 
     }
+
 
 
 
