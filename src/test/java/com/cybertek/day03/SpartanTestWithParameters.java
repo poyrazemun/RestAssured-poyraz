1762230a60c9db2ebbf7f8152ee45fbaeb1a2dac
poyrazemun <ofatih-poyraz@hotmail.com>
2022-07-13 20:10:59 +0200

    day03 PathParam and QueryParam

---
+++ src/test/java/com/cybertek/day03/SpartanTestWithParameters.java
@@ -0,0 +1,109 @@
+package com.cybertek.day03;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+
+
+public class SpartanTestWithParameters {
+
+    @BeforeAll
+    public static void init() {
+        //save baseurl inside this variable so that we dont need to type each http method.
+        baseURI = "http://3.83.68.127:8000";
+    }
+
+
+     /*   Given accept type is Json
+          And Id parameter value is 5
+          When user sends GET request to /api/spartans/{id}
+          Then response status code should be 200
+          And response content-type: application/json
+          And "Blythe" should be in response payload
+       */
+
+    @DisplayName("GET request to /api/spartans/{id} with ID 5")
+    @Test
+    public void test1() {
+
+        Response response = given().accept(ContentType.JSON).and().pathParam("id", 5)
+                .when().get("/api/spartans/{id}");
+
+
+        Assertions.assertEquals(200, response.statusCode());
+
+        Assertions.assertEquals("application/json", response.contentType());
+
+        Assertions.assertTrue(response.body().asString().contains("Blythe"));
+
+    }
+
+     /*
+        TASK
+        Given accept type is Json
+        And Id parameter value is 500
+        When user sends GET request to /api/spartans/{id}
+        Then response status code should be 404
+        And response content-type: application/json
+        And "Not Found" message should be in response payload
+     */
+
+    @DisplayName("GET request /api/spartans/{id} Negative Test")
+    @Test
+    public void test2() {
+
+        Response response = given().accept(ContentType.JSON).and().pathParam("id", 500)
+                .when().get("/api/spartans/{id}");
+
+        Assertions.assertEquals(404, response.statusCode());
+
+        Assertions.assertEquals("application/json", response.contentType());
+
+        Assertions.assertTrue(response.body().asString().contains("Not Found"));
+
+
+    }
+
+
+     /*
+        Given accept type is Json
+        And query parameter values are:
+        gender|Female
+        nameContains|e
+        When user sends GET request to /api/spartans/search
+        Then response status code should be 200
+        And response content-type: application/json
+        And "Female" should be in response payload
+        And "Janette" should be in response payload
+     */
+
+    @DisplayName("GET request to /api/spartans/search with Query Params")
+    @Test
+    public void test3() {
+
+        Response response = given().log().all().accept(ContentType.JSON).and().queryParam("gender", "Female")
+                .and().queryParam("nameContains", "e").when()
+                .get("/api/spartans/search");
+
+        Assertions.assertEquals(200, response.statusCode());
+        Assertions.assertEquals("application/json", response.contentType());
+        Assertions.assertTrue(response.body().asString().contains("Female"));
+        Assertions.assertTrue(response.body().asString().contains("Janette"));
+
+
+    }
+
+    @DisplayName("GET request to /api/spartans/search with Query Params (MAP)")
+    @Test
+    public void test4() {
+
+
+    }
+
+
+}
