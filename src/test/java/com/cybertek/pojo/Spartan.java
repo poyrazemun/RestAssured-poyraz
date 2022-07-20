94f9991ded3a6dd02b2dc09d91106f00bcf09da4
poyrazemun <ofatih-poyraz@hotmail.com>
2022-07-20 18:08:42 +0200

    Pojo fixed

--- src/test/java/com/cybertek/pojo/Spartan.java
+++ src/test/java/com/cybertek/pojo/Spartan.java
@@ -1,52 +1,20 @@
 package com.cybertek.pojo;
 
-public class Spartan {
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@JsonIgnoreProperties(value = "id", allowSetters = true)
+//bunu true yaptigim icin post request yaparken body'de id gonderilmeyecek, yani serialize yapilirken id kullanilmayacak
+public class Spartan {
 
     private int id;
     private String name;
     private String gender;
     private long phone;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public long getPhone() {
-        return phone;
-    }
-
-    public void setPhone(long phone) {
-        this.phone = phone;
-    }
-
-    @Override
-    public String toString() {
-        return "Spartan{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", gender='" + gender + '\'' +
-                ", phone=" + phone +
-                '}';
-    }
 }
