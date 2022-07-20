a1b799f9984693371375949528740f4d4ef52b83
poyrazemun <ofatih-poyraz@hotmail.com>
2022-07-20 17:29:11 +0200

    Spartan Post Request 2nd Way MAP TO JSON (using Serialization)

--- src/test/java/com/cybertek/day07/SpartanPostRequest.java
+++ src/test/java/com/cybertek/day07/SpartanPostRequest.java
@@ -5,7 +5,12 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import com.cybertek.utilities.SpartanTestBase;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
@@ -49,6 +54,37 @@ public class SpartanPostRequest extends SpartanTestBase {
         assertThat(response.path("data.name"),is("Christian Troy"));
         assertThat(response.path("data.gender"),is("Male"));
         assertThat(response.path("data.phone"),is(2356458965l));
+
+    }
+
+    @DisplayName("POST with Map to JSON")
+    @Test
+    public void postMethod2(){
+
+
+        //create a map to keep request json body information
+
+        Map<String,Object> requestJsonMap = new LinkedHashMap<>();
+        requestJsonMap.put("name","Kimber");
+        requestJsonMap.put("gender","Female");
+        requestJsonMap.put("phone",85256623333l);
+
+        Response response = given().accept(ContentType.JSON).and().contentType(ContentType.JSON)
+                .body(requestJsonMap).log().all()
+                .when().post("/api/spartans");
+
+        assertThat(response.statusCode(),is(201));
+        assertThat(response.contentType(),is("application/json"));
+
+        String expectedResponseMessage = "A Spartan is Born!";
+
+        assertThat(response.path("success"),is(expectedResponseMessage));
+
+        assertThat(response.path("data.name"),is("Kimber"));
+        assertThat(response.path("data.gender"),is("Female"));
+        assertThat(response.path("data.phone"),is(85256623333l));
+
+
 
     }
 
